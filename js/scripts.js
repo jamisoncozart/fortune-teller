6a2118a4a0fddd4dbeb2a9f3b6aaeec38f0e1cb9
Jamison Cozart and Zakkrey Short <zakkrey@>
2020-01-22 13:06:58 -0800

    add conditionals to display different fortunes.

--- js/scripts.js
+++ js/scripts.js
@@ -5,17 +5,23 @@ $(document).ready(function(){
     event.preventDefault();
     $("#panelgood").show();
     $("input:checkbox[name=luckyevent]:checked").each(function(){
-      
       goodcheck += parseInt($(this).val());
-
     })
-    console.log(goodcheck);
-
+    console.log(goodcheck)
     $("#panelbad").show();
     $("input:checkbox[name=badevent]:checked").each(function(){
-      
       badcheck += parseInt($(this).val());
-
     })
+    console.log(badcheck);
+    if (goodcheck > badcheck){
+      $("#panelgood").show();
+      $("#panelbad").hide();
+    } else if (badcheck > goodcheck){
+      $("#panelbad").show();
+      $("#panelgood").hide();
+    } else {
+      $("#panelbad").show();
+      $("#panelgood").show();
+    }
   })
 })
